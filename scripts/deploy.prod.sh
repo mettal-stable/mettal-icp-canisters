187820d323897f425b2a2eab0b634260332a78f3
Ferso <erickfernando@gmail.com>
2024-11-24 08:41:49 -0600

    fixed deploy

---
+++ scripts/deploy.prod.sh
@@ -0,0 +1,10 @@
+
+#controller_id dev
+# export CONTROLLER_IDhxdrg-c5lyh-bmfna-xyae5-fv6tq-lj6mb-xsubn-spk2v-lmwxy-ww5bq-uqe
+
+#controller_id prod
+#export CONTROLLER_ID=5j56q-yerxa-yvyft-lt6dp-gasf3-gaptg-7s2og-2ecrm-u5qb7-crbc4-sae
+
+dfx deps deploy 
+dfx generate
+dfx deploy 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,9 @@
 
+#controller_id dev
+#hxdrg-c5lyh-bmfna-xyae5-fv6tq-lj6mb-xsubn-spk2v-lmwxy-ww5bq-uqe
+
+#controller_id prod
+#5j56q-yerxa-yvyft-lt6dp-gasf3-gaptg-7s2og-2ecrm-u5qb7-crbc4-sae
 
 dfx deps deploy 
 dfx generate
